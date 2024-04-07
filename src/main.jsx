aab245c62f32d3008ec4af68355b693cb144200d
Hammad-Hasnain <hammadhasnain81@gmail.com>
2024-04-08 02:21:08 +0500

    hoo_bank done

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Provider } from 'react-redux'
-import STORE, { persistor } from './store/index.js'
-import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={STORE}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider >
+  <App />
 )
